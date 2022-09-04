bb13c2f9217119ca814ddd6b0721562b2836122d
James Jung <therealjamesjung@gmail.com>
2022-09-04 15:55:05 +0900

    feat: Set enum for page props

--- src/components/subPage/index.tsx
+++ src/components/subPage/index.tsx
@@ -1,27 +1,27 @@
 import * as S from "./styled";
-import * as T from "./type";
+import { PageEnum, SubPageProps } from "./type";
 import { AiOutlineMenu } from "react-icons/ai";
 import { Page15 } from "../page1.5";
 import { Page2 } from "../page2";
 
-export const SubPage = ({ page }: T.SubPageProps) => {
+export const SubPage = ({ page }: SubPageProps) => {
   const renderContent = () => {
-    if (page === "1.5") return <Page15 />;
-    else if (page === "2") return <Page2 />;
+    if (page === PageEnum.CODE) return <Page15 />;
+    else if (page === PageEnum.LIVE) return <Page2 />;
   };
   return (
     <S.SubPageWrapper>
-      <S.SubPageHeader>
-        <S.SubPageResponsiveHeader>
+      <S.SubPageHeaderWrapper>
+        <S.SubPageHeader>
           <S.LogoBox>LOLex</S.LogoBox>
           <AiOutlineMenu size="24" color="white" />
-        </S.SubPageResponsiveHeader>
-      </S.SubPageHeader>
-      <S.SubPageContent>
-        <S.SubPageContentResponse>
+        </S.SubPageHeader>
+      </S.SubPageHeaderWrapper>
+      <S.SubPageContentWrapper>
+        <S.SubPageContent>
           <>{renderContent()}</>
-        </S.SubPageContentResponse>
-      </S.SubPageContent>
+        </S.SubPageContent>
+      </S.SubPageContentWrapper>
     </S.SubPageWrapper>
   );
 };
