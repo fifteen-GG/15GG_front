747bbb580ec6e1e7eb37f75f64fb176ec6cac429
Jinhyung Lee <luciancah@gmail.com>
2022-09-20 03:26:00 +0900

    refactor: minor component changes

--- src/components/dataCode/components/Header.tsx
+++ src/components/dataCode/components/Header.tsx
@@ -3,9 +3,9 @@ import { Title, SubTitle } from '../styles/header.s';
 
 export const Header = () => {
   return (
-    <div>
+    <>
       <Title>데이터코드 생성</Title>
       <SubTitle>데이터내셔에서 여섯자리 코드를 입력해주세요.</SubTitle>
-    </div>
+    </>
   );
 };
--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -1,7 +1,65 @@
 import React from 'react';
-import * as S from './index.s';
 import { useNavigate } from 'react-router-dom';
+import { SearchBox } from './components/SearchBox';
 import { LiveGame } from './components/LiveGame';
+import styled from 'styled-components';
+import Bg from '../../assets/background.png';
+import logo from '../../assets/logo.png';
+
+const LandingWrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  background-position: center;
+  background-image: url(${Bg});
+`;
+
+const TitleWrapper = styled.div`
+  width: 314px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  margin-top: 180px;
+  width: 150px;
+  height: 33px;
+  background-position: center;
+  background-image: url(${logo});
+`;
+
+const GamesWrapper = styled.div`
+  width: 314px;
+  margin-top: 64px;
+`;
+
+const GamesTitleWrapper = styled.div`
+  width: 314px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const GamesTitle = styled.div`
+  color: #fff;
+  font-size: 16;
+  font-weight: 500;
+`;
+
+const CodeButton = styled.div`
+  width: 67px;
+  height: 25px;
+  background-color: #4d4ae2;
+  border-radius: 8px;
+  display: flex;
+  color: #fff;
+  font-size: 10pt;
+  font-weight: 500;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
 
 export const Landing = () => {
   const navigate = useNavigate();
@@ -10,25 +68,23 @@ export const Landing = () => {
   };
 
   return (
-    <S.LandingWrapper>
-      <S.TitleWrapper>
-        <S.Logo />
-        <S.SearchBox>
-          <S.SearchRegion>KR</S.SearchRegion>
-        </S.SearchBox>
-        <S.GamesWrapper>
-          <S.GamesTitleWrapper>
-            <S.GamesTitle>분석중인 경기</S.GamesTitle>
-            <S.CodeButton onClick={routeNewCode}>코드 생성</S.CodeButton>
-          </S.GamesTitleWrapper>
+    <LandingWrapper>
+      <TitleWrapper>
+        <Logo />
+        <SearchBox />
+        <GamesWrapper>
+          <GamesTitleWrapper>
+            <GamesTitle>분석중인 경기</GamesTitle>
+            <CodeButton onClick={routeNewCode}>코드 생성</CodeButton>
+          </GamesTitleWrapper>
           {/* todo margintop 12, bottom 64 */}
           <LiveGame />
           <LiveGame />
           <LiveGame />
           <LiveGame />
           <LiveGame />
-        </S.GamesWrapper>
-      </S.TitleWrapper>
-    </S.LandingWrapper>
+        </GamesWrapper>
+      </TitleWrapper>
+    </LandingWrapper>
   );
 };
