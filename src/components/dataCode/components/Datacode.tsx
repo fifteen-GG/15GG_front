29f3f3bca69c4a6d3ed366aa3716fc431000e2fa
Jinhyung Lee <luciancah@gmail.com>
2022-10-03 02:11:35 +0900

    feat: Get new datacode from API server

--- src/components/dataCode/components/Datacode.tsx
+++ src/components/dataCode/components/Datacode.tsx
@@ -1,20 +1,53 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
   DataCodeWrapper,
   SingleDataCodeWrapper,
   SingleDataCode,
+  Footer,
+  FooterContent,
+  RefreshButton,
 } from '../styles/datacode.s';
-const code = [7, 2, 1, 9, 3, 4];
 
 export const Datacode = () => {
+  const [code, setCode] = useState([0, 0, 0, 0, 0, 0]);
+  const [expireTime, setExpireTime] = useState(0);
+  const [refresh, setRefresh] = useState(0);
+  let currentTime = new Date().getTime();
+
+  const getNewCode = async () => {
+    try {
+      const value = await axios.get(`${process.env.REACT_APP_GG_API_ROOT}code`);
+      console.log(value);
+      if (value.status === 200) {
+        setCode(value.data.value.split(''));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getNewCode();
+  }, [refresh]);
+
   return (
-    <DataCodeWrapper>
-      {code.map((value: number, index: number) => {
-        return (
-          <SingleDataCodeWrapper key={index}>
-            <SingleDataCode>{value}</SingleDataCode>
-          </SingleDataCodeWrapper>
-        );
-      })}
-    </DataCodeWrapper>
+    <>
+      <DataCodeWrapper>
+        {code.map((value: number, index: number) => {
+          return (
+            <SingleDataCodeWrapper key={index}>
+              <SingleDataCode>{value}</SingleDataCode>
+            </SingleDataCodeWrapper>
+          );
+        })}
+      </DataCodeWrapper>
+      <Footer>
+        <FooterContent>남은시간 2:21 · </FooterContent>
+        <RefreshButton onClick={() => setRefresh(data => data + 1)}>
+          재생성
+        </RefreshButton>
+      </Footer>
+    </>
   );
 };
--- src/components/dataCode/index.tsx
+++ src/components/dataCode/index.tsx
@@ -12,30 +12,12 @@ const DataCodeResponsive = styled.div`
   width: 328px;
 `;
 
-const Footer = styled.div`
-  margin-top: 10px;
-  width: 328px;
-  text-align: right;
-  display: flex;
-  flex-direction: row;
-  justify-content: right;
-`;
-
-const FooterContent = styled.div`
-  font-size: 12px;
-  font-weight: 400;
-  color: #ffffff;
-`;
-
 export const DataCode = () => {
   return (
     <Page15Wrapper>
       <DataCodeResponsive>
         <Header />
         <Datacode />
-        <Footer>
-          <FooterContent>남은시간 2:21 · 재생성</FooterContent>
-        </Footer>
       </DataCodeResponsive>
     </Page15Wrapper>
   );
