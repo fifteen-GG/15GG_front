6cc6265baf095a6cbef17fac738aadb26c3eb3ca
7ngenious <zxvs1218@gmail.com>
2022-10-17 16:52:46 +0900

    chore: [OD15-90] Edit Summoner.tsx by referencing code review

--- src/components/gameAnalysis/components/Summoner.tsx
+++ src/components/gameAnalysis/components/Summoner.tsx
@@ -19,34 +19,11 @@ import {
   ItemWrapper,
 } from '../styles/summoners.s';
 
-import {
-  urlChampion,
-  urlSpell,
-  urlItem,
-  formatPerks,
-  formatPerkStyles,
-} from '../../utils/Url';
-import { TeamInfo } from '../styles/teamInfoContainer.s';
-import { useState } from 'react';
-
-const formatKDA = (props: {
-  kills: number;
-  deaths: number;
-  assists: number;
-}) => {
-  return `${props.kills}  /  ${props.deaths}  /  ${props.assists} `;
-};
-
-// export interface Team {
-//   redTeam: summonerProps[];
-//   blueTeam: summonerProps[];
-// }
-export type TeamInfoProps = {
-  teamInfo: summonerProps;
+const formatKDA = (kills: number, deaths: number, assists: number) => {
+  return `${kills}  /  ${deaths}  /  ${assists} `;
 };
 
 export interface summonerProps {
-  // team: 'red' | 'blue';
   champion: string;
   championSpell1: string;
   championSpell2: string;
@@ -55,19 +32,29 @@ export interface summonerProps {
   items: string[];
 }
 
-const Summoner = ({ teamInfo }: TeamInfoProps) => {
+const Summoner = (props: { teamInfo: summonerProps }) => {
   return (
     <PlayerWrapper>
       <UserInterface>
-        <ChampionImg src={urlChampion(teamInfo.champion)} />
+        <ChampionImg
+          src={`${process.env.REACT_APP_DDRAGON_API_ROOT}/champion/${props.teamInfo.champion}.png`}
+        />
         <ChampionLevel>16</ChampionLevel>
         <SpellWrapper>
-          <Spell src={urlSpell(teamInfo.championSpell1)} />
-          <Spell src={urlSpell(teamInfo.championSpell2)} />
+          <Spell
+            src={`${process.env.REACT_APP_DDRAGON_API_ROOT}/spell/${props.teamInfo.championSpell1}.png`}
+          />
+          <Spell
+            src={`${process.env.REACT_APP_DDRAGON_API_ROOT}/spell/${props.teamInfo.championSpell2}.png`}
+          />
         </SpellWrapper>
         <PerksWrapper>
-          <Perk src={formatPerks(teamInfo.perks)} />
-          <Perk src={formatPerkStyles(teamInfo.perkStyles)} />
+          <Perk
+            src={`${process.env.REACT_APP_OPGG_API_ROOT}/lol/perk/${props.teamInfo.perks}.png`}
+          />
+          <Perk
+            src={`${process.env.REACT_APP_OPGG_API_ROOT}/lol/perkStyle/${props.teamInfo.perkStyles}.png`}
+          />
         </PerksWrapper>
         <UserInfoWrapper>
           <SummonerInfo>
@@ -75,22 +62,23 @@ const Summoner = ({ teamInfo }: TeamInfoProps) => {
             <SummonerTier>P4</SummonerTier>
           </SummonerInfo>
           <KDAWrapper>
-            <KDADetails>
-              {formatKDA({ kills: 15, deaths: 6, assists: 5 })}
-            </KDADetails>
+            <KDADetails>{formatKDA(15, 6, 5)}</KDADetails>
             <KDA>KDA 3.3</KDA>
           </KDAWrapper>
         </UserInfoWrapper>
       </UserInterface>
       <ItemInterface>
         <ItemWrapper>
-          {teamInfo.items.map((item, index) => {
+          {props.teamInfo.items.map((item, index) => {
             return index !== 3 ? (
-              <ItemImg src={urlItem(item)} key={index} />
+              <ItemImg
+                src={`${process.env.REACT_APP_DDRAGON_API_ROOT}/item/${item}.png`}
+                key={index}
+              />
             ) : (
               <ItemImg
                 className="item3"
-                src={urlItem(teamInfo.items[3])}
+                src={`${process.env.REACT_APP_DDRAGON_API_ROOT}/item/${props.teamInfo.items[3]}.png`}
                 key={index}
               />
             );
